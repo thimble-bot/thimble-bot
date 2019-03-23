5285a718e8a1bda8af228c0cfe4847aad127e1cc
József Sallai <jozsef@sallai.me>
2019-03-23 12:18:26 +0200

    add bee movie command

---
+++ commands/fun/BeeMovie.js
@@ -0,0 +1,27 @@
+const { Command } = require('discord.js-commando');
+const fs = require('fs');
+const path = require('path');
+
+class BeeMovieCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'beemovie',
+      group: 'fun',
+      memberName: 'beemovie',
+      description: 'Get a random sentence from the Bee Movie script.'
+    });
+  }
+
+  run(message) {
+    const contents = fs.readFileSync(path.join(__dirname, 'BeeMovie', 'script.txt'), {
+      encoding: 'utf8'
+    });
+
+    const paragraphs = contents.split('\n\n');
+    const randomParagraph = paragraphs[Math.floor(Math.random() * paragraphs.length)];
+
+    return message.say(`${randomParagraph}.`);
+  }
+};
+
+module.exports = BeeMovieCommand;
